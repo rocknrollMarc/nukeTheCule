8df13dc41064eb9326a1b7859bae78d6e9641f46
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-13 19:50:58 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NukeTheCule::Application.config.secret_token = '374f17a0e41681854508f2238dccfc5ad62570d6ec44e53e61ac269f81fee0de84550d40d9538788fd6115669987e9a2fabab60c0791d1c2de83303de6732493'
